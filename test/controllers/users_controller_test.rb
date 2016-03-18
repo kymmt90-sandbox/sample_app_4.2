054eb18102f5854bca6670adbcd2afb7c81c1c76
Kohei Yamamoto <kymmt90@gmail.com>
2016-03-18 11:35:33 +0900

    Add admin attribute test (solves exercise 3)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -56,4 +56,13 @@ class UsersControllerTest < ActionController::TestCase
     end
     assert_redirected_to root_path
   end
+
+  test 'should not allow the admin attribute to be edited via Web' do
+    log_in_as @other_user
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password: @other_user.password,
+                                            password_confirmation: @other_user.password_confirmation,
+                                            admin: true }
+    assert_not @other_user.reload.admin?
+  end
 end
