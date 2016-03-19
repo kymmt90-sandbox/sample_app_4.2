ee61525557aa3dbedf9138746f374a59e66c8741
Kohei Yamamoto <kymmt90@gmail.com>
2016-03-19 20:35:02 +0900

    Add microposts count test (solves exercise 11.2)

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -33,4 +33,21 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:hogefuga))
     assert_select 'a', text: 'delete', count: 0
   end
+
+  test 'micropost sidebar count' do
+    log_in_as @user
+
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+
+    other_user = users(:mallory)
+    log_in_as other_user
+
+    get root_path
+    assert_match '0 microposts', response.body
+
+    other_user.microposts.create!(content: 'A micropost')
+    get root_path
+    assert_match /1 micropost\b/, response.body
+  end
 end
